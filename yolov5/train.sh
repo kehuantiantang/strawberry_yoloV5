8153c4fc3135fdf8a2b2c983ee92f695e4c37d3c
sober <741446285@qq.com>
2020-12-16 20:49:17 +0900

    yolovV5 docker, random degree, multi-scale, synbatchnorm

--- yolov5/train.sh
+++ yolov5/train.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python train.py --batch-size 32 --img-size 419 --cfg '../strawberry_params/sb_model_larger.yaml'
+python train.py --batch-size 32 --img-size 419 --sync-bn --multi-scale --workers 5 --batch-size 28
