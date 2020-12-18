ff49b481b3833764231ba26dd2ed857af35c064b
sober <741446285@qq.com>
2020-12-18 21:06:00 +0900

    yoloV5_l version test

--- yolov5/train.sh
+++ yolov5/train.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-python train.py --img-size 419 --sync-bn --multi-scale --workers 5 --batch-size 28 \
- --cfg "../strawberry_params/sb_model_larger.yaml" --epochs 500 --hyp "../strawberry_params/hyp.strawberry_large.yaml"
+python train.py --img-size 419 --sync-bn --workers 8 --batch-size 64 \
+ --cfg "../strawberry_params/sb_model_l.yaml" --epochs 500 --hyp "../strawberry_params/hyp.strawberry.yaml" \
+--weight "../dataset/yolov5l.pt" --multi-scale
