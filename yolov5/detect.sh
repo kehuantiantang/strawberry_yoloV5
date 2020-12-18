2e0ce01314dbb9bd4ec8e160934d3a780511727a
sober <741446285@qq.com>
2020-12-18 09:46:00 +0900

    exp_907aug_894_deeper

--- yolov5/detect.sh
+++ yolov5/detect.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python detect.py --img-size 419 --source "../dataset/make/test" --weights "./output/train/exp_913aug_885/weights/best.pt" --augment
+python detect.py --img-size 419 --source "../dataset/make/test" --weight "./output/train/exp_907aug_894_deeper/weights/best.pt" --augment
--- yolov5/test.sh
+++ yolov5/test.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-python test.py --data "../strawberry_params/strawberry.yaml" --img-size 419 --iou-thres 0.5 --weight "./output/train/exp_913aug_885/weights/best.pt" --verbose
+python test.py --data "../strawberry_params/strawberry.yaml" --img-size 419 \
+--iou-thres 0.5 --weight "./output/train/exp_907aug_894_deeper/weights/best.pt" "./output/train/exp_913aug_885/weights/best.pt" --verbose
